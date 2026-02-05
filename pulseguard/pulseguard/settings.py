ada11456f6f9167694545d85d6dd1357fce3e87c
gleiria <g.n.leiria@exeter.ac.uk>
2026-02-05 07:22:25 +0000

    handshake between Postgres and Django

--- pulseguard/pulseguard/settings.py
+++ pulseguard/pulseguard/settings.py
@@ -72,10 +72,21 @@ WSGI_APPLICATION = 'pulseguard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pulseguard',
+        'USER': 'gnl202',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
